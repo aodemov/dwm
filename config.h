b8e6cb746fa09390d8521070833fdfa023537dd2
sam <contact.samhurwitz@gmail.com>
2021-03-02 13:47:03 +0500

    fix tag name

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
